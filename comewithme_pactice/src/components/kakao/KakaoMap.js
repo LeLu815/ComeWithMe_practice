c1c00e2c177dbef66676e338ce2fb33ab0b986f5
LinQuixote <leein9404@naver.com>
2023-12-17 01:03:03 +0900

    feature : 서울시 구별 지도 폴리곤 구현

--- comewithme_pactice/src/components/kakao/KakaoMap.js
+++ comewithme_pactice/src/components/kakao/KakaoMap.js
@@ -1,19 +1,120 @@
-import React, { useEffect } from "react";
-import geoJson from "../../data/sig.json";
+import React, { useEffect, useState } from "react";
+import geoJson from "../../data/TL_SCCO_CTPRVN.json";
+import outerGeoJson from "../../data/OUTER.json";
 
 const { kakao } = window;
 
 const KakaoMap = () => {
-  const data = geoJson.features;
-  console.log(data);
+  const [clickData, setClickData] = useState(null);
+
+  const areas = [];
+  const features = geoJson.features;
+  for (let i = 0; i < features.length; i++) {
+    const name = features[i].properties.SIG_KOR_NM;
+    const path = features[i].geometry.coordinates[0].map(
+      (numList) => new kakao.maps.LatLng(Number(numList[1]), Number(numList[0]))
+    );
+    areas.push({
+      name: name,
+      path: path,
+    });
+  }
+
+  const outerAreas = [];
+  const outerFeatures = outerGeoJson.features;
+  for (let i = 0; i < outerFeatures.length; i++) {
+    const name = outerFeatures[i].properties.SIG_KOR_NM;
+    const path = outerFeatures[i].geometry.coordinates[0].map(
+      (numList) => new kakao.maps.LatLng(Number(numList[1]), Number(numList[0]))
+    );
+    outerAreas.push({
+      name: name,
+      path: path,
+    });
+  }
 
   useEffect(() => {
+    // const data = geoJson.features;
+
     const container = document.getElementById("map");
     const options = {
       center: new kakao.maps.LatLng(37.5642135, 127.0016985), // 이미지 지도의 중심좌표
       level: 9,
     };
-    const map = new kakao.maps.StaticMap(container, options);
+    const map = new kakao.maps.Map(container, options);
+
+    // const customOverlay = new kakao.maps.CustomOverlay({});
+    // const infowindow = new kakao.maps.InfoWindow({ removable: true });
+
+    function displayArea(area) {
+      // 다각형을 생성합니다
+      new kakao.maps.Polygon({
+        map: map, // 다각형을 표시할 지도 객체
+        path: area.path,
+        strokeWeight: 2,
+        strokeColor: "#646464",
+        strokeOpacity: 1,
+        fillColor: "#1a1a1a",
+        fillOpacity: 1,
+      });
+    }
+    function outerDisplayArea(area) {
+      new kakao.maps.Polygon({
+        map: map, // 다각형을 표시할 지도 객체
+        path: area.path,
+        strokeColor: "#646464",
+        strokeOpacity: 1,
+        fillColor: "#646464",
+        fillOpacity: 1,
+      });
+    }
+    //   // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경합니다
+    //   // 지역명을 표시하는 커스텀오버레이를 지도위에 표시합니다
+    //   kakao.maps.event.addListener(polygon, "mouseover", function (mouseEvent) {
+    //     polygon.setOptions({ fillColor: "#09f" });
+
+    //     customOverlay.setContent('<div class="area">' + area.name + "</div>");
+
+    //     customOverlay.setPosition(mouseEvent.latLng);
+    //     customOverlay.setMap(map);
+    //   });
+
+    //   // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다
+    //   kakao.maps.event.addListener(polygon, "mousemove", function (mouseEvent) {
+    //     customOverlay.setPosition(mouseEvent.latLng);
+    //   });
+
+    //   // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래색으로 변경합니다
+    //   // 커스텀 오버레이를 지도에서 제거합니다
+    //   kakao.maps.event.addListener(polygon, "mouseout", function () {
+    //     polygon.setOptions({ fillColor: "#fff" });
+    //     customOverlay.setMap(null);
+    //   });
+
+    //   // 다각형에 click 이벤트를 등록하고 이벤트가 발생하면 다각형의 이름과 면적을 인포윈도우에 표시합니다
+    //   kakao.maps.event.addListener(polygon, "click", function (mouseEvent) {
+    //     var content =
+    //       '<div class="info">' +
+    //       '   <div class="title">' +
+    //       area.name +
+    //       "</div>" +
+    //       '   <div class="size">총 면적 : 약 ' +
+    //       Math.floor(polygon.getArea()) +
+    //       " m<sup>2</sup></div>" +
+    //       "</div>";
+
+    //     infowindow.setContent(content);
+    //     infowindow.setPosition(mouseEvent.latLng);
+    //     infowindow.setMap(map);
+    //   });
+    // }
+
+    for (var i = 0; i < areas.length; i++) {
+      displayArea(areas[i]);
+    }
+    for (var i = 0; i < outerAreas.length; i++) {
+      outerDisplayArea(outerAreas[i]);
+    }
   }, []);
   return <div id="map" style={{ width: "700px", height: "500px" }}></div>;
 };
